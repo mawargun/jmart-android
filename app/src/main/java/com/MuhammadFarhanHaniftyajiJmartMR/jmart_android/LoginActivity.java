a3345e3d4ecee0dcdd7255b37df396d2d66d4f1b
Muhammad Farhan Haniftyaji <farhan201591@gmail.com>
2021-11-27 02:28:15 +0700

    TP - Modul 9 - Setting

---
+++ app/src/main/java/com/MuhammadFarhanHaniftyajiJmartMR/jmart_android/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.MuhammadFarhanHaniftyajiJmartMR.jmart_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
