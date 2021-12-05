6ca9f92f8fa096067bf2960fbcc45d5acaa39216
Muhammad Farhan Haniftyaji <farhan201591@gmail.com>
2021-12-05 23:51:16 +0700

    Tutam - Modul 9 - pengaturan

---
+++ app/src/main/java/com/MuhammadFarhanHaniftyajiJmartMR/jmart_android/AboutMeActivity.java
@@ -0,0 +1,22 @@
+package com.MuhammadFarhanHaniftyajiJmartMR.jmart_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AboutMeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_me);
+        TextView name = (TextView) findViewById(R.id.nameOutputAccount);
+        name.setText("" + LoginActivity.getLoggedAccount().name);
+
+        TextView email = (TextView) findViewById(R.id.emailOutputAccount);
+        email.setText("" + LoginActivity.getLoggedAccount().email);
+
+        TextView balance = (TextView) findViewById(R.id.balanceOutputAccount);
+        balance.setText("" + LoginActivity.getLoggedAccount().balance);
+    }
+
+}
--- app/src/main/java/com/MuhammadFarhanHaniftyajiJmartMR/jmart_android/MainActivity.java
+++ app/src/main/java/com/MuhammadFarhanHaniftyajiJmartMR/jmart_android/MainActivity.java
@@ -1,16 +1,58 @@
 package com.MuhammadFarhanHaniftyajiJmartMR.jmart_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView userMain = findViewById(R.id.textHelloUser);
-        userMain.setText("Welcome " + LoginActivity.getLoggedAccount().name);
+
+        tabLayout = findViewById(R.id.productTab);
+        viewPager = findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new ProductFragment(), "PRODUCT");
+        vpAdapter.addFragment(new FilterFragment(), "FILTER");
+        viewPager.setAdapter(vpAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.account_button) {
+            Toast.makeText(this, "Account Selected", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, AboutMeActivity.class);
+            startActivity(intent);
+        }
+        if (item.getItemId() == R.id.add_button) {
+            Toast.makeText(this, "Create Product Selected", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, CreateProductActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
